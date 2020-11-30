ce8d7bf4c250fe368b646b2233ee3f714b7a6952
Isa <isami.dono@gmail.com>
2020-12-01 01:49:37 +0300

    Museum: - Flat test - house test

---
+++ Museum/Assets/Script/btnCtrl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnCtrl : MonoBehaviour
+{
+    public GameObject house;
+    public GameObject flat;
+    void Start()
+    {
+        
+    }
+
+    public void onHouseClick()
+    {
+        house.SetActive(true);
+        flat.SetActive(false);
+    }
+
+    public void onFlatClick()
+    {
+        house.SetActive(false);
+        flat.SetActive(true);
+    }
+}
